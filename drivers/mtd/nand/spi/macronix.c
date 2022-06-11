2bdb32d8f5a7aceb35c8f072f6cdb812df59e0e1
ZhaoWei Xu <paldier@hotmail.com>
2022-06-11 10:18:31 +0800

    add spinand support for mt7621

--- drivers/mtd/nand/spi/macronix.c
+++ drivers/mtd/nand/spi/macronix.c
@@ -16,6 +16,19 @@
 #define SPINAND_MFR_MACRONIX		0xC2
 #define MACRONIX_ECCSR_MASK		0x0F
 
+#if defined(CONFIG_BOARD_MT7621_SPINAND_RFB)
+#define SPINAND_PAGE_READ_FROM_CACHE_OP_NOR_EMU(fast, addr, ndummy, buf, len)	\
+	SPI_MEM_OP(SPI_MEM_OP_CMD(fast ? 0x0b : 0x03, 1),		\
+		   SPI_MEM_OP_ADDR(3, addr, 1),				\
+		   SPI_MEM_OP_DUMMY(ndummy, 1),				\
+		   SPI_MEM_OP_DATA_IN(len, buf, 1))
+
+static SPINAND_OP_VARIANTS(read_cache_variants_nor_emu,
+		SPINAND_PAGE_READ_FROM_CACHE_X4_OP(0, 1, NULL, 0),
+		SPINAND_PAGE_READ_FROM_CACHE_X2_OP(0, 1, NULL, 0),
+		SPINAND_PAGE_READ_FROM_CACHE_OP_NOR_EMU(true, 0, 1, NULL, 0),
+		SPINAND_PAGE_READ_FROM_CACHE_OP_NOR_EMU(false, 0, 0, NULL, 0));
+#endif
 
 static SPINAND_OP_VARIANTS(read_cache_variants,
 		SPINAND_PAGE_READ_FROM_CACHE_X4_OP(0, 1, NULL, 0),
@@ -122,6 +135,48 @@ static const struct spinand_info macronix_spinand_table[] = {
 					      &update_cache_variants),
 		     SPINAND_HAS_QE_BIT,
 		     SPINAND_ECCINFO(&mx35lfxge4ab_ooblayout, NULL)),
+	SPINAND_INFO("MX35LF2GE4AD", 0x26,
+		     NAND_MEMORG(1, 2048, 64, 64, 2048, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&mx35lfxge4ab_ooblayout,
+				     mx35lf1ge4ab_ecc_get_status)),
+	SPINAND_INFO("MX35LF4GE4AD", 0x37,
+		     NAND_MEMORG(1, 4096, 128, 64, 2048, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&mx35lfxge4ab_ooblayout,
+				     mx35lf1ge4ab_ecc_get_status)),
+	SPINAND_INFO("MX35LF1G24AD", 0x14,
+		     NAND_MEMORG(1, 2048, 128, 64, 1024, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&mx35lfxge4ab_ooblayout, NULL)),
+	SPINAND_INFO("MX35LF2G24AD", 0x24,
+		     NAND_MEMORG(1, 2048, 128, 64, 2048, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&mx35lfxge4ab_ooblayout, NULL)),
+	SPINAND_INFO("MX35LF4G24AD", 0x35,
+		     NAND_MEMORG(1, 4096, 256, 64, 2048, 2, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&mx35lfxge4ab_ooblayout, NULL)),
 	SPINAND_INFO("MX35UF4GE4AD", 0xb7,
 		     NAND_MEMORG(1, 4096, 256, 64, 2048, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
@@ -170,10 +225,192 @@ static const struct spinand_info macronix_spinand_table[] = {
 
 };
 
+#if defined(CONFIG_BOARD_MT7621_SPINAND_RFB)
+#define TABLE_SZ (ARRAY_SIZE(macronix_spinand_table))
+static struct spinand_info macronix_spinand_nor_emu_table[TABLE_SZ];
+
+static void macronix_spinand_generate_nor_emu_table(void)
+{
+	static struct spinand_info *info = macronix_spinand_nor_emu_table;
+	unsigned int i;
+
+	memcpy(macronix_spinand_nor_emu_table, macronix_spinand_table,
+	       sizeof(macronix_spinand_table));
+
+	for (i = 0; i < TABLE_SZ; i++) {
+		info[i].op_variants.read_cache = &read_cache_variants_nor_emu;
+		info[i].flags |= SPINAND_RELOAD_PAGE_0;
+	}
+}
+
+static int macronix_spinand_read_cfg_reg(struct spinand_device *spinand, u8 *buf,
+				       u8 *val)
+{
+	struct spi_mem_op op = SPINAND_GET_FEATURE_OP(REG_CFG, buf);
+	int ret;
+
+	ret = spi_mem_exec_op(spinand->spimem, &op);
+	if (ret)
+		return ret;
+
+	*val = buf[0];
+	return 0;
+}
+
+static int macronix_spinand_write_cfg_reg(struct spinand_device *spinand, u8 *buf,
+					u8 val)
+{
+	struct spi_mem_op op = SPINAND_SET_FEATURE_OP(REG_CFG, buf);
+
+	buf[0] = val;
+	return spi_mem_exec_op(spinand->spimem, &op);
+}
+
+static int macronix_spinand_load_page(struct spinand_device *spinand,
+				    unsigned int row)
+{
+	struct spi_mem_op op = SPINAND_PAGE_READ_OP(row);
+
+	return spi_mem_exec_op(spinand->spimem, &op);
+}
+
+static int macronix_spinand_read_from_cache(struct spinand_device *spinand,
+					  void *buf, unsigned int len)
+{
+	struct spi_mem_op op = SPINAND_PAGE_READ_FROM_CACHE_OP_NOR_EMU(false, 0,
+								0, NULL, 0);
+	unsigned int nbytes = len;
+	u8 *bufptr = buf;
+	int ret;
+
+	/*
+	 * Some controllers are limited in term of max RX data size. In this
+	 * case, just repeat the READ_CACHE operation after updating the
+	 * column.
+	 */
+	while (nbytes) {
+		op.data.buf.in = bufptr;
+		op.data.nbytes = nbytes;
+		ret = spi_mem_adjust_op_size(spinand->spimem, &op);
+		if (ret)
+			return ret;
+
+		ret = spi_mem_exec_op(spinand->spimem, &op);
+		if (ret)
+			return ret;
+
+		bufptr += op.data.nbytes;
+		nbytes -= op.data.nbytes;
+		op.addr.val += op.data.nbytes;
+	}
+
+	return 0;
+}
+
+static int macronix_spinand_detect_nor_emu(struct spinand_device *spinand,
+					 u8 devid, int *enabled)
+{
+	struct device *dev = &spinand->spimem->spi->dev;
+	unsigned int i, num_ffs = 0, num_zeros = 0, len = 0;
+	u8 cfg, *buf;
+	int ret;
+
+	for (i = 0; i < TABLE_SZ; i++) {
+		const struct spinand_info *info = &macronix_spinand_table[i];
+
+		if (devid != info->devid)
+			continue;
+
+		len = info->memorg.pagesize + info->memorg.oobsize;
+		break;
+	}
+
+	if (!len) {
+		*enabled = 0;
+		return 0;
+	}
+
+	buf = kmalloc(len, GFP_KERNEL);
+	if (!buf)
+		return -ENOMEM;
+
+	ret = macronix_spinand_read_cfg_reg(spinand, buf, &cfg);
+	if (ret) {
+		dev_err(dev, "failed to read configuration\n");
+		goto cleanup;
+	}
+
+	cfg &= ~0xc2;
+	cfg |= 0x82;
+
+	ret = macronix_spinand_write_cfg_reg(spinand, buf, cfg);
+	if (ret) {
+		dev_err(dev, "failed to write configuration\n");
+		goto cleanup;
+	}
+
+	ret = macronix_spinand_read_cfg_reg(spinand, buf, &cfg);
+	if (ret) {
+		dev_err(dev, "failed to read back configuration\n");
+		goto cleanup;
+	}
+
+	if ((cfg & 0xc2) != 0x82) {
+		dev_info(dev,
+			 "flash chip does not support NOR read emulation\n");
+		ret = -ENOTSUPP;
+		goto cleanup;
+	}
+
+	ret = macronix_spinand_load_page(spinand, 0);
+	if (ret) {
+		dev_err(dev, "failed to load NOR read configuration\n");
+		goto cleanup;
+	}
+
+	ret = macronix_spinand_read_from_cache(spinand, buf, len);
+	if (ret) {
+		dev_err(dev, "failed to read NOR read configuration\n");
+		goto cleanup;
+	}
+
+	if (!ret) {
+		for (i = 0; i < len; i++) {
+			if (buf[i] == 0xff)
+				num_ffs++;
+			else if (!buf[i])
+				num_zeros++;
+		}
+
+		if (num_ffs == len) {
+			*enabled = 0;
+		} else if (num_zeros == len) {
+			*enabled = 1;
+		} else {
+			dev_warn(dev,
+				 "failed to check NOR read configuration\n");
+			ret = -EINVAL;
+		}
+	}
+
+cleanup:
+	if (macronix_spinand_write_cfg_reg(spinand, buf, cfg & (~0xc2)))
+		dev_err(dev, "failed to leave NOR read configuration\n");
+
+	kfree(buf);
+
+	return ret;
+}
+#endif
+
 static int macronix_spinand_detect(struct spinand_device *spinand)
 {
 	u8 *id = spinand->id.data;
 	int ret;
+#if defined(CONFIG_BOARD_MT7621_SPINAND_RFB)
+	struct device *dev = &spinand->spimem->spi->dev;
+	int nor_read = 0;
+#endif
 
 	/*
 	 * Macronix SPI NAND read ID needs a dummy byte, so the first byte in
@@ -181,7 +418,20 @@ static int macronix_spinand_detect(struct spinand_device *spinand)
 	 */
 	if (id[1] != SPINAND_MFR_MACRONIX)
 		return 0;
+#if defined(CONFIG_BOARD_MT7621_SPINAND_RFB)
 
+	ret = macronix_spinand_detect_nor_emu(spinand, id[2], &nor_read);
+	if (ret)
+		nor_read = 0;
+	if (nor_read) {
+		dev_info(dev, "flash chip is using NOR read emulation\n");
+		macronix_spinand_generate_nor_emu_table();
+		ret = spinand_match_and_init(spinand, macronix_spinand_nor_emu_table,
+				     TABLE_SZ,
+				     id[2]);
+	}else
+
+#endif
 	ret = spinand_match_and_init(spinand, macronix_spinand_table,
 				     ARRAY_SIZE(macronix_spinand_table),
 				     id[2]);
@@ -200,3 +450,4 @@ const struct spinand_manufacturer macronix_spinand_manufacturer = {
 	.name = "Macronix",
 	.ops = &macronix_spinand_manuf_ops,
 };
+
